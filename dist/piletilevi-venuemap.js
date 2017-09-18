1c59ddf55210d1d918905f677c4e9ba61a60e6ec
viljar <viljar@artweb.ee>
2017-09-18 15:58:37 +0300

    disabled sections seats excluding, map trimming

--- dist/piletilevi-venuemap.js
+++ dist/piletilevi-venuemap.js
@@ -810,6 +810,7 @@ piletilevi.venuemap = {
 	DEFAULT_SEAT_INACTIVE_COLOR: '#d0d0d0',
 	SEAT_CIRCLE_RADIUS: 6,
 	SEAT_CIRCLE_STROKE_WIDTH: 0.4,
+	STAGE_TEXT_SIZE: 20,
 	DEBUG_FULL_PLACESMAP_SECTIONS: false,
 };
 
@@ -897,6 +898,16 @@ piletilevi.venuemap.Utilities = new function() {
 		}
 		return result;
 	};
+	this.getSvgTextBBox = function(text, attributes) {
+		var svgElement = piletilevi.venuemap.Utilities.createSvgNode('svg');
+		var node = piletilevi.venuemap.Utilities.createSvgNode('text', attributes);
+		node.innerHTML = text;
+		svgElement.appendChild(node);
+		document.body.appendChild(svgElement);
+		var result = node.getBBox();
+		document.body.removeChild(svgElement);
+		return result;
+	};
 };
 
 piletilevi.venuemap.VenueMap = function() {
@@ -927,6 +938,7 @@ piletilevi.venuemap.VenueMap = function() {
 	var lastLoadedVenueSuccessful = false;
 	var seatsSections = {};
 	var requestCache = {};
+	var canvasFactory;
 
 	var seatColors = {
 		'hover': piletilevi.venuemap.DEFAULT_SEAT_HOVER_COLOR,
@@ -938,6 +950,7 @@ piletilevi.venuemap.VenueMap = function() {
 		componentElement = document.createElement('div');
 		componentElement.className = 'piletilevi_venue_map';
 		componentElement.style.display = 'none';
+		canvasFactory = new piletilevi.venuemap.VenuePlacesMapCanvasFactory(self);
 		self.hide();
 	};
 	var adjustToZoom = function() {
@@ -984,218 +997,10 @@ piletilevi.venuemap.VenueMap = function() {
 	};
 	var receiveVenuePlacesMap = function(response) {
 		var data = JSON.parse(response);
-		var svgElement = piletilevi.venuemap.Utilities.createSvgNode('svg', {
-			viewBox: '0 0 ' + data.width + ' ' + data.height,
-			width: '100%',
-			height: '100%',
-		});
-		var cssNode = piletilevi.venuemap.Utilities.createSvgNode('style');
-		cssNode.innerHTML =
-			'.seat_text {' +
-			'  text-shadow: 1px 1px rgba(0,0,0,0.25);' +
-			'}' +
-			'.place_detail {' +
-			'  display: none;' +
-			'}' +
-			'.with_seat_numbers .place_detail {' +
-			'  display: initial;' +
-			'}' +
-			'.section_label {' +
-			'  display: none;' +
-			'}' +
-			'.with_section_labels .section_label {' +
-			'  display: initial;' +
-			'}';
-		svgElement.appendChild(cssNode);
-		if (data.stageType) {
-			var node = piletilevi.venuemap.Utilities.createSvgNode('text', {
-				id: 'stagename',
-				'text-anchor': 'middle',
-				x: data.stageX,
-				y: data.stageY,
-				fill: '#999999',
-				'font-family': 'Arial',
-				'font-size': 20,
-				'font-weight': 'bold',
-			});
-			node.innerHTML = data.stageType;
-			svgElement.appendChild(node);
-		}
-		var sectionsSeats = {};
-		var rowEdges = {};
-		var rowEdgeStruct = function() {
-			this.firstSeat = null;
-			this.lastSeat = null;
-		};
-		for (var i = 0; i < data.seats.length; ++i) {
-			var seat = data.seats[i];
-			var section = seat.section;
-			if (!sectionsSeats[section]) {
-				sectionsSeats[section] = [];
-			}
-			var rowKey = section + '_' + seat.row;
-			if (!rowEdges[rowKey]) {
-				rowEdges[rowKey] = new rowEdgeStruct();
-			}
-			var rowEdge = rowEdges[rowKey];
-			if (!rowEdge.firstSeat || +rowEdge.firstSeat.place > +seat.place) {
-				rowEdge.firstSeat = seat;
-			}
-			if (!rowEdge.lastSeat || +rowEdge.lastSeat.place < +seat.place) {
-				rowEdge.lastSeat = seat;
-			}
-			sectionsSeats[section].push(seat);
-		}
-		var boundaries = {};
-		var placeFactor = piletilevi.venuemap.SEAT_CIRCLE_STROKE_WIDTH
-			+ piletilevi.venuemap.SEAT_CIRCLE_RADIUS;
-		var sectionLabelElements = {};
-		for (var sectionId in sectionsSeats) {
-			var topLeft = {
-				x: -1,
-				y: -1,
-			};
-			var bottomRight = {
-				x: -1,
-				y: -1,
-			};
-			for (var i = 0; i < sectionsSeats[sectionId].length; ++i) {
-				var seat = sectionsSeats[sectionId][i];
-				var x = +seat.x;
-				var y = +seat.y;
-				if (topLeft.x < 0 || x < topLeft.x) {
-					topLeft.x = x;
-				}
-				if (topLeft.y < 0 || y < topLeft.y) {
-					topLeft.y = y;
-				}
-				if (bottomRight.x < 0 || x > bottomRight.x) {
-					bottomRight.x = x;
-				}
-				if (bottomRight.y < 0 || y > bottomRight.y) {
-					bottomRight.y = y;
-				}
-			}
-			var sectionRegion = {
-				x: topLeft.x - placeFactor,
-				y: topLeft.y - placeFactor,
-				width: bottomRight.x - topLeft.x + placeFactor * 2,
-				height: bottomRight.y - topLeft.y + placeFactor * 2,
-			};
-			if (piletilevi.venuemap.DEBUG_FULL_PLACESMAP_SECTIONS) {
-				var node = piletilevi.venuemap.Utilities.createSvgNode('rect', {
-					x: sectionRegion.x,
-					y: sectionRegion.y,
-					width: sectionRegion.width,
-					height: sectionRegion.height,
-					fill: '#7f52ff',
-					'data-section': sectionId,
-				});
-				svgElement.appendChild(node);
-			}
-			var node = piletilevi.venuemap.Utilities.createSvgNode('text', {
-				'class': 'section_label',
-				x: sectionRegion.x + sectionRegion.width / 2,
-				y: sectionRegion.y - piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2,
-				dy: '-0.35em', // center align vertically
-				'stroke-width': 0,
-				'text-anchor': 'middle',  // center align horizontally
-				'font-family': 'Verdana',
-				'font-size': 14,
-				'fill': '#999999'
-			});
-			sectionLabelElements[sectionId] = node;
-			svgElement.appendChild(node);
-			boundaries[sectionId] = sectionRegion;
-		}
-		for (var key in rowEdges) {
-			var edge = rowEdges[key];
-			if (!edge.firstSeat || !edge.lastSeat) {
-				continue;
-			}
-			svgElement.appendChild(createRowNumberNode(edge.firstSeat, edge.lastSeat));
-			svgElement.appendChild(createRowNumberNode(edge.lastSeat, edge.firstSeat));
-		}
-		for (var i = 0; i < data.seats.length; ++i) {
-			var seat = data.seats[i];
-			var section = seat.section;
-			var groupNode = piletilevi.venuemap.Utilities.createSvgNode('g', {
-				'class': 'place',
-				id: 'place_' + seat.code,
-				cx: seat.x,
-				cy: seat.y,
-			});
-			svgElement.appendChild(groupNode);
-			var node = piletilevi.venuemap.Utilities.createSvgNode('circle', {
-				cx: seat.x,
-				cy: seat.y,
-				r: piletilevi.venuemap.SEAT_CIRCLE_RADIUS
-			});
-			groupNode.appendChild(node);
-			if (seat.place) {
-				node = piletilevi.venuemap.Utilities.createSvgNode('text', {
-					'class': 'place_detail seat_text',
-					x: seat.x,
-					y: seat.y,
-					dy: '0.35em', // center align vertically
-					'stroke-width': 0,
-					'text-anchor': 'middle',  // center align horizontally
-					'font-family': 'Verdana',
-					'font-size': 6,
-					'fill': 'white'
-				});
-				node.innerHTML = seat.place;
-				groupNode.appendChild(node);
-			}
-			if (!sectionsSeats[section]) {
-				sectionsSeats[section] = [];
-			}
-			sectionsSeats[section].push(seat);
-		}
-		var canvas = new piletilevi.venuemap.PlacesMapCanvas(self, svgElement, sectionLabelElements);
-		canvas.setSectionsBoundaries(boundaries);
+		var canvas = canvasFactory.createCanvas(data);
 		placesMap.changeCanvas(canvas);
 	};
-	var createRowNumberNode = function(seat1, seat2) {
-		var alignedLeft = seat1.x <= seat2.x;
-		var position = seat1.x;
-		if (alignedLeft) {
-			position -= piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2;
-		} else {
-			position += piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2;
-		}
-		var calculateAngle = function(x1, y1, x2, y2) {
-			return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-		};
-		var angle = Math.round(
-			alignedLeft
-				? calculateAngle(
-				seat1.x,
-				seat1.y,
-				seat2.x,
-				seat2.y
-			) : calculateAngle(
-				seat2.x,
-				seat2.y,
-				seat1.x,
-				seat1.y
-			));
 
-		var node = piletilevi.venuemap.Utilities.createSvgNode('text', {
-			'class': 'place_detail',
-			x: position,
-			y: seat1.y,
-			dy: '0.35em', // center align vertically
-			'transform': 'rotate(' + angle + ',' + seat1.x + ',' + seat1.y + ')',
-			'stroke-width': 0,
-			'text-anchor': alignedLeft ? 'end' : 'start',  // center align horizontally
-			'font-family': 'Verdana',
-			'font-size': 10,
-			'fill': '#999999'
-		});
-		node.innerHTML = seat1.row;
-		return node;
-	};
 	var loadSectionPlacesMap = function(sectionId, onSuccess, onFail) {
 		self.requestShopData(
 			'/public/upload/seatingplan_section_svg/' + confId + '_' + sectionId + '.svg',
@@ -1897,7 +1702,9 @@ piletilevi.venuemap.PlacesMapCanvas = function(venueMap, svgElement, sectionLabe
 		}
 		var label = sectionLabelElements[sectionDetails.id] && sectionDetails.title
 			? sectionDetails.title : '';
-		sectionLabelElements[sectionDetails.id].innerHTML = label;
+		if (sectionLabelElements[sectionDetails.id]) {
+			sectionLabelElements[sectionDetails.id].innerHTML = label;
+		}
 		var priceClasses = sectionDetails.priceClasses || [];
 		var priceClassesIndex = {};
 		for (var i = priceClasses.length; i--;) {
@@ -2380,5 +2187,276 @@ piletilevi.venuemap.PlaceTooltip = function(venueMap) {
 		if (componentElement) {
 			componentElement.style.display = 'none';
 		}
+	};
+};
+
+piletilevi.venuemap.VenuePlacesMapCanvasFactory = function(venueMap) {
+	this.createCanvas = function(data) {
+		var svgElement = piletilevi.venuemap.Utilities.createSvgNode('svg', {
+			viewBox: '0 0 ' + data.width + ' ' + data.height,
+			width: '100%',
+			height: '100%',
+		});
+		var cssNode = piletilevi.venuemap.Utilities.createSvgNode('style');
+		cssNode.innerHTML =
+			'.seat_text {' +
+			'  text-shadow: 1px 1px rgba(0,0,0,0.25);' +
+			'}' +
+			'.place_detail {' +
+			'  display: none;' +
+			'}' +
+			'.with_seat_numbers .place_detail {' +
+			'  display: initial;' +
+			'}' +
+			'.section_label {' +
+			'  display: none;' +
+			'}' +
+			'.with_section_labels .section_label {' +
+			'  display: initial;' +
+			'}';
+		svgElement.appendChild(cssNode);
+		var node;
+		var sectionsSeats = {};
+		var rowEdges = {};
+		var rowEdgeStruct = function() {
+			this.firstSeat = null;
+			this.lastSeat = null;
+		};
+		var relevantSeats = [];
+		var enabledSections = venueMap.getEnabledSections();
+		var enabledSectionsIndex = {};
+		for (var i = enabledSections.length; i--;) {
+			enabledSectionsIndex[enabledSections[i]] = true;
+		}
+		for (var i = 0; i < data.seats.length; ++i) {
+			var seat = data.seats[i];
+			var section = seat.section;
+			if (!enabledSectionsIndex[section]) {
+				continue;
+			}
+
+			if (!sectionsSeats[section]) {
+				sectionsSeats[section] = [];
+			}
+			var rowKey = section + '_' + seat.row;
+			if (!rowEdges[rowKey]) {
+				rowEdges[rowKey] = new rowEdgeStruct();
+			}
+			var rowEdge = rowEdges[rowKey];
+			if (!rowEdge.firstSeat || +rowEdge.firstSeat.place > +seat.place) {
+				rowEdge.firstSeat = seat;
+			}
+			if (!rowEdge.lastSeat || +rowEdge.lastSeat.place < +seat.place) {
+				rowEdge.lastSeat = seat;
+			}
+			sectionsSeats[section].push(seat);
+			relevantSeats.push(seat);
+		}
+		// trim the map
+		var mapRegion = calculateSeatsRegion(relevantSeats);
+		var padding = piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 4;
+		mapRegion.x -= padding;
+		mapRegion.y -= padding;
+		mapRegion.width += padding *2;
+		mapRegion.height += padding *2;
+		if (data.stageType) {
+			var textSize = piletilevi.venuemap.Utilities.getSvgTextBBox(venueMap.getTranslation('stage-' + data.stageType), {
+				'font-family': 'Arial',
+				'font-size': piletilevi.venuemap.STAGE_TEXT_SIZE,
+				'font-weight': 'bold',
+			});
+			var textX = data.stageX - textSize.width / 2;
+			var textY = data.stageY - textSize.height / 2;
+			if (textX < mapRegion.x) {
+				mapRegion.width += mapRegion.x - textX;
+				mapRegion.x = textX;
+			}
+			if (textY < mapRegion.y) {
+				mapRegion.height += mapRegion.y - textY;
+				mapRegion.y = textY;
+			}
+			if (mapRegion.width < textX + textSize.width) {
+				mapRegion.width = textX + textSize.width;
+			}
+			if (mapRegion.height < textY + textSize.height) {
+				mapRegion.height = textY + textSize.height;
+			}
+		}
+		svgElement.setAttribute('viewBox', '0 0 ' + mapRegion.width + ' ' + mapRegion.height);
+		data.stageX -= mapRegion.x;
+		data.stageY -= mapRegion.y;
+		for (var i = relevantSeats.length; i--;) {
+			var seat = relevantSeats[i];
+			seat.x -= mapRegion.x;
+			seat.y -= mapRegion.y;
+		}
+		if (data.stageType) {
+			node = piletilevi.venuemap.Utilities.createSvgNode('text', {
+				id: 'stagename',
+				'text-anchor': 'middle',
+				x: data.stageX,
+				y: data.stageY,
+				fill: '#999999',
+				'font-family': 'Arial',
+				'font-size': piletilevi.venuemap.STAGE_TEXT_SIZE,
+				'font-weight': 'bold',
+				'dy': '0.3em',
+			});
+			node.innerHTML = data.stageType;
+			svgElement.appendChild(node);
+		}
+		var boundaries = {};
+		var sectionLabelElements = {};
+		for (var sectionId in sectionsSeats) {
+			var sectionRegion = calculateSeatsRegion(sectionsSeats[sectionId]);
+			if (piletilevi.venuemap.DEBUG_FULL_PLACESMAP_SECTIONS) {
+				var node = piletilevi.venuemap.Utilities.createSvgNode('rect', {
+					x: sectionRegion.x,
+					y: sectionRegion.y,
+					width: sectionRegion.width,
+					height: sectionRegion.height,
+					fill: '#7f52ff',
+					'data-section': sectionId,
+				});
+				svgElement.appendChild(node);
+			}
+			var node = piletilevi.venuemap.Utilities.createSvgNode('text', {
+				'class': 'section_label',
+				x: sectionRegion.x + sectionRegion.width / 2,
+				y: sectionRegion.y - piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2,
+				dy: '-0.35em', // center align vertically
+				'stroke-width': 0,
+				'text-anchor': 'middle',  // center align horizontally
+				'font-family': 'Verdana',
+				'font-size': 14,
+				'fill': '#999999'
+			});
+			sectionLabelElements[sectionId] = node;
+			svgElement.appendChild(node);
+			boundaries[sectionId] = sectionRegion;
+		}
+		for (var key in rowEdges) {
+			var edge = rowEdges[key];
+			if (!edge.firstSeat || !edge.lastSeat) {
+				continue;
+			}
+			svgElement.appendChild(createRowNumberNode(edge.firstSeat, edge.lastSeat));
+			svgElement.appendChild(createRowNumberNode(edge.lastSeat, edge.firstSeat));
+		}
+		for (var i = 0; i < relevantSeats.length; ++i) {
+			var seat = relevantSeats[i];
+			var section = seat.section;
+			var groupNode = piletilevi.venuemap.Utilities.createSvgNode('g', {
+				'class': 'place',
+				id: 'place_' + seat.code,
+				cx: seat.x,
+				cy: seat.y,
+			});
+			svgElement.appendChild(groupNode);
+			var node = piletilevi.venuemap.Utilities.createSvgNode('circle', {
+				cx: seat.x,
+				cy: seat.y,
+				r: piletilevi.venuemap.SEAT_CIRCLE_RADIUS
+			});
+			groupNode.appendChild(node);
+			if (seat.place) {
+				node = piletilevi.venuemap.Utilities.createSvgNode('text', {
+					'class': 'place_detail seat_text',
+					x: seat.x,
+					y: seat.y,
+					dy: '0.35em', // center align vertically
+					'stroke-width': 0,
+					'text-anchor': 'middle',  // center align horizontally
+					'font-family': 'Verdana',
+					'font-size': 6,
+					'fill': 'white'
+				});
+				node.innerHTML = seat.place;
+				groupNode.appendChild(node);
+			}
+			if (!sectionsSeats[section]) {
+				sectionsSeats[section] = [];
+			}
+			sectionsSeats[section].push(seat);
+		}
+		var canvas = new piletilevi.venuemap.PlacesMapCanvas(venueMap, svgElement, sectionLabelElements);
+		canvas.setSectionsBoundaries(boundaries);
+		return canvas;
+	};
+	var calculateSeatsRegion = function(seats) {
+		var topLeft = {
+			x: -1,
+			y: -1,
+		};
+		var bottomRight = {
+			x: -1,
+			y: -1,
+		};
+		for (var i = 0; i < seats.length; ++i) {
+			var seat = seats[i];
+			var x = +seat.x;
+			var y = +seat.y;
+			if (topLeft.x < 0 || x < topLeft.x) {
+				topLeft.x = x;
+			}
+			if (topLeft.y < 0 || y < topLeft.y) {
+				topLeft.y = y;
+			}
+			if (bottomRight.x < 0 || x > bottomRight.x) {
+				bottomRight.x = x;
+			}
+			if (bottomRight.y < 0 || y > bottomRight.y) {
+				bottomRight.y = y;
+			}
+		}
+		var placeFactor = piletilevi.venuemap.SEAT_CIRCLE_STROKE_WIDTH
+			+ piletilevi.venuemap.SEAT_CIRCLE_RADIUS;
+		var result = {
+			x: topLeft.x - placeFactor,
+			y: topLeft.y - placeFactor,
+			width: bottomRight.x - topLeft.x + placeFactor * 2,
+			height: bottomRight.y - topLeft.y + placeFactor * 2,
+		};
+		return result;
+	};
+	var createRowNumberNode = function(seat1, seat2) {
+		var alignedLeft = seat1.x <= seat2.x;
+		var position = seat1.x;
+		if (alignedLeft) {
+			position -= piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2;
+		} else {
+			position += piletilevi.venuemap.SEAT_CIRCLE_RADIUS * 2;
+		}
+		var calculateAngle = function(x1, y1, x2, y2) {
+			return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+		};
+		var angle = Math.round(
+			alignedLeft
+				? calculateAngle(
+				seat1.x,
+				seat1.y,
+				seat2.x,
+				seat2.y
+			) : calculateAngle(
+				seat2.x,
+				seat2.y,
+				seat1.x,
+				seat1.y
+			));
+
+		var node = piletilevi.venuemap.Utilities.createSvgNode('text', {
+			'class': 'place_detail',
+			x: position,
+			y: seat1.y,
+			dy: '0.35em', // center align vertically
+			'transform': 'rotate(' + angle + ',' + seat1.x + ',' + seat1.y + ')',
+			'stroke-width': 0,
+			'text-anchor': alignedLeft ? 'end' : 'start',  // center align horizontally
+			'font-family': 'Verdana',
+			'font-size': 10,
+			'fill': '#999999'
+		});
+		node.innerHTML = seat1.row;
+		return node;
 	};
 };
