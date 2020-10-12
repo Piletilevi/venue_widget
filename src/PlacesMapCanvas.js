afb43a211285ec0c1dd90e891c5eb15acb8451e0
moroz1999 <moroz1999@gmail.com>
2020-10-12 16:39:15 +0300

    Selected tickets displaying

--- src/PlacesMapCanvas.js
+++ src/PlacesMapCanvas.js
@@ -160,7 +160,7 @@ export default function PlacesMapCanvas(venueMap, svgElement, sectionLabelElemen
             let seatPriceClass = priceClassesIndex[seatInfo.priceClass] || null;
             let selectable = seatsManuallySelectable && seatInfo;
             placeObject.setPriceClass(seatPriceClass);
-            placeObject.setSelected(venueMap.isSeatSelected(seatInfo.id));
+            placeObject.setSelected(venueMap.isSeatSelected(seatInfo.id) || seatInfo.status === Constants.STATUS_SELECTED);
             placeObject.allowManuallySelectable(selectable);
             placeObject.allowSeatsStatusDisplaying(venueMap.isSeatsStatusDisplayed());
             placeObject.refreshStatus();
